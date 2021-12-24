b516ac8263f32d5552c8679284047f04ac68f583
Danila-Fedortsov <61584036+Danila-Fedortsov@users.noreply.github.com>
2021-12-24 13:32:16 +0300

    Update calculator.js

--- n5/calculator.js
+++ n5/calculator.js
@@ -1,12 +1,25 @@
-function click1() {
-    let a = document.getElementsByName("price");
-    let b = document.getElementsByName("num");
-    if ((/^(0|[1-9]\d*)$/.test(a[0].value)) && (/^(0|[1-9]\d*)$/.test(b[0].value))) {
-        let r = document.getElementById("res");
-        r.innerHTML = a[0].value * b[0].value;
-        return false;
+function calc() {
+    let pattern = /^[0-9]+$/;
+    let text = 'Some1 text with 4 words and 1 plus ^0^';
+    let price = document.getElementsByName("price")[0],
+        count = document.getElementsByName("num")[0],
+        result = document.getElementById("res"),
+        priceSum = 0,
+        countSum = 0,
+        totalResult = 0;
+
+    console.log(price.value, count.value);
+
+    if (pattern.test(price.value) && pattern.test(count.value)) {
+        result.innerHTML = price.value * count.value;
     } else {
-        alert("Ты ввел что-то непонятное");
-        return false;
+        alert("Некорректное заначение")
     }
+    return false;
 }
+
+window.addEventListener('DOMContentLoaded', function(event) {
+    console.log("DOM fully loaded and parsed");
+    let b = document.getElementById("button1");
+    b.addEventListener("click", calc);
+});
